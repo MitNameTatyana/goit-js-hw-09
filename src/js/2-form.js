efab84928af0bfaca4802b63e0f12d249a55df0f
Tatyana Chuiko <chyikotatyana@gmail.com>
2024-06-20 16:10:55 +0200

    done hw9

---
+++ src/js/2-form.js
@@ -0,0 +1,48 @@
+const LS_Key = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+form.addEventListener('input', evt => {
+  formData.email = form.elements.email.value;
+  formData.message = form.elements.message.value;
+
+  saveToLS(LS_Key, formData);
+});
+
+window.addEventListener('DOMContentLoaded', evt => {
+  const formData = loadFromLS(LS_Key);
+  form.elements.email.value = formData?.email ?? '';
+  form.elements.message.value = formData?.message ?? '';
+});
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  formData.email = form.elements.email.value.trim();
+  formData.message = form.elements.message.value.trim();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+  }
+
+  console.log(formData);
+  localStorage.removeItem(LS_Key);
+  form.reset();
+});
+
+function saveToLS(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+function loadFromLS(key) {
+  const json = localStorage.getItem(key);
+  try {
+    const formData = JSON.parse(json);
+    return formData;
+  } catch (error) {
+    return json;
+  }
+}
